f1ea3a2cabcaee2301b2b7be0cc3062b88e7de0a
Raphi3z1 <leekunfeng2015@gmail.com>
2021-11-28 01:17:55 +0800

    a working editprofilepicture.php

--- editprofilepicture.php
+++ editprofilepicture.php
@@ -1,21 +1,19 @@
 <?php include 'process_editprofilepicture.php'?>
-<!doctype html>
-<html lang="en">
+<link rel="stylesheet" href="css/profilepage.css"/>
     <?php
     include "head.inc.php"
     ?>
     <body>
         <?php
         include "nav.inc.php";
-        session_start();
         // get current user details
-        $userResults = getUserFromID($_SESSION['userID']);
+        $userID = $_SESSION['userID'];
         ?>
 
         <main class="container-fluid vh-100" style="margin-top:100px">
             <div class="" style="margin-top:100px">
                 <div class="rounded d-flex justify-content-center">
-                    <div class="col-md-4 col-sm-12 shadow-lg p-5 bg-light">
+                    <div class="col-md-10 col-sm-12 shadow-lg p-5 bg-light">
                         <div class="text-center">
                             <h3 class="text-primary">Profile Picture Selection</h3>
                         </div>
@@ -24,31 +22,26 @@
                                 <?php echo $msg; ?>
                             </div>
                         <?php endif; ?>
-                        <form action="process_editprofilepicture.php" method="post" enctype="multipart/form-data">
+                        <form action="editprofilepicture.php" method="post" enctype="multipart/form-data">
                             <div class="col-4 offset-md-4 form-div"> 
-                                <div class="form-group">
+                                <div class="form-group text-center">
+                                    <img src="images/default.jpg" onclick="click()" id="profileDisplay" />
                                     <label for="profileImage"> Profile Image </label>
-                                    <input class="form-control" type="file" id="profileImage" name="profileImage">
+                                    <input class="form-control" type="file" id="profileImage" onchange="displayImage(this)" name="profileImage">
                                 </div>
-                                
-                                <div class="input-group mb-3">
-                                    <span class="input-group-text bg-primary text-white"><i
-                                            class="bi bi-key-fill text-white"></i> Biography</span>
-                                    <input class="form-control" type="text" id="biography" name="biography" value="<?php echo $userResults['biography']; ?>">
-                                </div>
-                                <button class="btn btn-primary text-center mt-2" type="submit" name="save-user">
+                                <div class="form-group text-center">
+                                <button class="btn btn-primary text-center" type="submit" name="save-user">
                                     Save picture
                                 </button>
-                            </div>
+                                </div>
                         </form>
                     </div>
                 </div>
             </div>
         </main>
-
-
+        <script src="js/profilepic.js"></script>
+    
     </body>
     <?php
     include "footer.inc.php"
     ?>
-</html>
--- process_editprofilepicture.php
+++ process_editprofilepicture.php
@@ -12,60 +12,62 @@
             
                 if (isset($_POST['save-user']))
                 {
-                  $img_tmpName = $_FILES['profileImage']['tmp_name'];
-                  $img = (file_get_contents($img_tmpName));
+                  $profileImagename = $userID . '_.jpg';
                   
-                  $stmt = $conn->prepare("UPDATE users SET profilePic = ? WHERE userID = ?");
-                  $stmt->bind_param("bi", $img, $userID);
-                  $stmt->send_long_data(0, $img);
-                  $stmt->execute();
-                  echo $stmt->error;
+                  $target = "images/" . $profileImagename;
+
+                  if (move_uploaded_file($_FILES['profileImage']['tmp_name'], $target))
+                  { //$sql to write into DB
+                    $sql = "UPDATE users SET profilePic = ('$profileImagename') WHERE userID = ('$userID')";
                   
-                  if (!$stmt->error) 
-                  {
-                    $msg = "Upload success";
-                    $css_class = "alert-success";
-                    header("Location: editProfile.php");
-                  } 
-                  else 
-                  {
-                
-                    $_SESSION['error'] = $stmt->error;
-                      $msg = "Failed to upload";
-                      $css_class = "alert-danger";
-                      header("Location: editprofilepicture.php");
+                    if (mysqli_query($conn, $sql))
+                    {
+                        $msg = $sql;
+                        $css_class = "alert-success";
+                    }
+                    else
+                    {
+                        $msg = "Failed to upload to database";
+                        $css_class = "alert-danger";
+                    }
+                    
                   }
+                  else
+                  {
+                    $msg = "Failed to upload";
+                    $css_class = "alert-danger";
+                    $success=false;
+                  }
+                }
                     
                     
                     
                   //echo "<pre>", print_r($_POST), "</pre>";
 //                  echo "<pre>", print_r($_FILES), "</pre>";
                   //echo "<pre>", print_r($_FILES['profileImage']), "</pre>";
-//                  $profileImagename = time() . '_' . $_FILES['profileImage']['name'];
+                    
+//                  $img_tmpName = $_FILES['profileImage']['tmp_name'];
+//                  $img = (file_get_contents($img_tmpName));
 //                  
-//                  $target = "images/" . $profileImagename;
-//
-//                  if (move_uploaded_file($_FILES['profileImage']['tmp_name'], $target))
-//                  { //$sql to write into DB
-//                    $sql = "UPDATE users SET profilePic = ('$profileImagename') WHERE userID = ('$userResults')";
+//                  $stmt = $conn->prepare("UPDATE users SET profilePic = ? WHERE userID = ?");
+//                  $stmt->bind_param("bi", $img, $userID);
+//                  $stmt->send_long_data(0, $img);
+//                  $stmt->execute();
+//                  echo $stmt->error;
 //                  
-//                    if (mysqli_query($conn, $sql))
-//                    {
-//                        $msg = $sql;
-//                        $css_class = "alert-success";
-//                    }
-//                    else
-//                    {
-//                        $msg = "Failed to upload to database";
-//                        $css_class = "alert-danger";
-//                    }
-//                    
-//                  }
-//                  else
+//                  if (!$stmt->error) 
 //                  {
-//                    $msg = "Failed to upload";
-//                    $css_class = "alert-danger";
-//                    $success=false;
+//                    $msg = "Upload success";
+//                    $css_class = "alert-success";
+//                    header("Location: editProfile.php");
+//                  } 
+//                  else 
+//                  {
+//                
+//                    $_SESSION['error'] = $stmt->error;
+//                      $msg = "Failed to upload";
+//                      $css_class = "alert-danger";
+//                      header("Location: editprofilepicture.php");
 //                  }
-                }
+                  
             ?>
