f004f06bb65854a4d299d3d9674a3dffaa9f22ad
Raphi3z1 <leekunfeng2015@gmail.com>
2021-11-29 12:38:47 +0800

    Added a footer customization

--- footer.inc.php
+++ footer.inc.php
@@ -1,3 +1,106 @@
-<footer class="container footer">
-    <p><em>Copyright &copy; 2021 Blog Post Pte. Ltd.</em></p>
+<!--<footer class="container footer">-->
+<!--    <p><em>Copyright &copy; 2021 Blog Post Pte. Ltd.</em></p>-->
+    
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+    <!-- Section: Social media -->
+    <section
+        class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+        >
+        <!-- Left -->
+        <div class="me-5 d-none d-lg-block">
+            <span>Get connected with us on social networks:</span>
+        </div>
+        <!-- Left -->
+        <!-- Right -->
+        <div>
+            <a href="https://www.facebook.com/" class="me-4 text-reset">
+                <i class="material-icons", style="font-size:3em">facebook</i>
+            </a>
+            <a href="https://www.instagram.com/" class="me-4 text-reset">
+                <i class="material-icons", style="font-size:3em">photo_camera</i>
+            </a>
+        </div>
+        <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="">
+        <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.665403029678!2d103.84659295176749!3d1.3774333989904735!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da16e96db0a1ab%3A0x3d0be54fbbd6e1cd!2sSingapore%20Institute%20of%20Technology%20(SIT%40NYP)!5e0!3m2!1sen!2ssg!4v1636465892564!5m2!1sen!2ssg" width="100%"></iframe>
+            </div>
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <i class="material-icons", style="font-size:3em">face</i>Stori
+                    </h6>
+                    <p>
+                        Post your Stori. Share with your friends
+                    </p>
+                </div>
+                <!-- Grid column -->
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Navigation
+                    </h6>
+                    <p>
+                        <a href="index.php" class="text-reset">Home</a>
+                    </p>
+                    <p>
+                        <a href="about.php" class="text-reset">About Us</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Useful links
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset">Help</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        FAQ
+                    </h6>
+                    <p><i class="material-icons", title="Address", style="font-size:2em">home</i> 
+                        10 Dover Dr, Singapore 138683
+                    </p>
+                    <p>
+                        <i class="material-icons", title="Email Contact",
+		style="font-size:2em">email</i>
+                        Stori@gmail.com
+                    </p>
+                    <p><i class="material-icons", title="Phone Contact", style="font-size:2em">phone</i> 
+                        + 65 1234 5678
+                    </p>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+        © 2021 Copyright:
+        <p>Copy Right &copy; 2021 Stori Pte. Ltd.</p>
+    </div>
+    <!-- Copyright -->
 </footer>
+<!-- Footer -->
+<!--</footer>-->
